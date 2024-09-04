cc201ce80a58002446eb9f68e66c2c89eea8e20a
Fepozopo <fepozopo.f39@gmail.com>
2024-09-03 19:11:09 -0700

    Update main.py to restrict game to draw a maximum of 60 times per second

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ def main():
     # Initialize pygame
     pygame.init()
 
-    # Print a few things to the console
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    # Set up the clock
+    clock = pygame.time.Clock()
+    dt = 0
 
     # Create the screen
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
@@ -21,13 +20,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            # Use the screen's fill method to fill the screen with a solid "black" color.
-            screen.fill((0, 0, 0))
-            # Use pygame's display.flip() method to refresh the screen.
-            pygame.display.flip()
 
-        # Update
-        pygame.display.update()
+        # Use the screen's fill method to fill the screen with a solid "black" color.
+        screen.fill((0, 0, 0))
+        # Use pygame's display.flip() method to refresh the screen.
+        pygame.display.flip()
+
+        # Update the delta time
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
